8f846a37b48ec7a9f8939d57547b53e3e803b3fb
mizterwaffles <nhillhouse04@gmail.com>
2025-10-21 12:03:48 -0600

    Initialized project

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*
+comment Add a comment to the top of your source code with the names of the authors, the date, and the name of the lab. 
+(Remember to commit the change.)
+
+greeting Print a greeting and description of the program to the user. 
+(Remember to commit the change.)
+
+Collect a string: Get a string from the user to manipulate, then split it into an array of words.
+
+Convert each word into Pig Latin: When looking at the first character of each word, 
+consider the command 'contains' it may save you some trouble
+
+Print the phrase in Pig Latin
+
+Get an offset: You can just assign an offset of '5', but a random number generator is so much more interesting. 
+Random rand = new Random(); int randomOffset = rand.Next(1,26); should help you get a random number between 1 and 25 (26 is just a full lap)
+
+Offset each character of each word: 
+char letter = (char)97; //will make letter into an 'a'.  
+letter = (char)((int)letter + 1); // if letter is an 'a', it will become a 'b'
+
+Print out the offset phrase
+*/
